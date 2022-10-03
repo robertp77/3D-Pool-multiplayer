de9f8ee0f58ae0af1597403a4cc28d972f3ac507
robertp77 <robert.a.paul99@gmail.com>
2022-10-03 15:35:42 -0800

    test

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-
+const auth = require('./authentication.js');
+auth.setupAuthentication(app);
+const { OAUTH } = require('./secrets.js');
+const cors = require('cors');
+const DataStore = require('./DataStore.js');
+const corsGitlab = cors({
+  origin: 'https://compsci290_2021spring.dukecs.io',
+  credentials: true,
+});
+app.use(corsGitlab);
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
---
+++ secrets.js
@@ -0,0 +1,19 @@
+exports.FIREBASE_CONFIG = {
+  "type": "service_account",
+  "project_id": "dpool-e2d7a",
+  "private_key_id": "83f455d9316ec044721973361d5e166e6c5bf23b",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDY9AZMI4Tt/3gp\ndbseUlHZPs3aKrJa8Czm5EO5YNn6ktv/ZFSIPTj+XMmcORT4/rWhyRBQuoqp87rI\nZ+weztMTZi24GbhgtX3lZ5b38duUi4qLEFUVpHCXYLYipECs84JRR6MkzelHA0jR\nGDKaQJurwPD71P9f2Z7YlLeNGc78peI065KsRKxf8l8sUtPimA2SQHgJ97f/gxre\np9hGE+SaYYYixjOJ+whwZs1vEAG4DK7QmlmjM16pTNzW15EX+rZ/pACoGgU65FAb\nybc+JbKk7eVqPYPkebmc6WU5mZQfPy3LHqYyslVT2vLFG9p7wqGI1e/7073pvJef\nipwTNOStAgMBAAECggEAPU9k0jJ9d+aotgq7yPtZfwvFD8DeK13RSqS6UNKCd1H2\nUuuvCdtHVnaxUPE+sIkGmkRgVHgLBLFImSbpLuGi+8n15J98H/RaMjC+lK4oMAfp\nLJh+ewHAAb+GaS7Cyj18l+ynOZjpMjTG6LQuZLIabAS39WsBijxNLAaHApyHBpVz\nKP/IacSz8hikUpPtBdRvufXyFdrC38S7wSoU0XAUHC+5bkwuMaM5sXsm4szDLbXt\nefQEVwM5QXp3G8CJrFixaA9+XPqdxb5+G83kuizssqd1LOrLbFiug7k1AK4dYrO0\nk5GXf8v5PAX86qmaVX6hnz9p7xWOT2AVlhr8GYC+3wKBgQDwD3vKs7PHbH9RSs28\nloHx2goL5FVHU40Z+GaQOWbITK2exMDUyyA06IjRLR55ogVS7DfxMejaKSNaiVbu\nGkAluEZqJZy3DnDoKZyBmTZcEBNse417BGIFqWnxbke0L5UO7Rg5hGUdDJyFIOVN\nYGrGqNo9HGgj7ACCBMAqxjAx5wKBgQDnW8RQYLeS3mO9cA+q1A8expl1zgeOPNjd\nhaiZHktgtSAJJjJ64j+bzy398dG2Yox7J0WMpnViaVhTHdclXeMRpkhins/l+z0s\n5+nRQdwiMWl7iGFy7TyB35xxIouTceRkeUU56Yube4nNpo5E2uq5fQGsqNectxNP\n22c2+9rKSwKBgQCdFFSZP1r8u9bXV44lv4oJufVm8KKIiHhErRBXgOfrWpkk0XOo\nCb3qQNn9nXtadZTsPJsiXNkTn2V9wW47YLvmspyIqlHQSkfA8jFxb9JtDSvYiUP7\nYdPOBpvHGbUbhyMO4D9Aq4wKJQ1p4yuHpPZZRXWAFU1ja/KtIPSpSIOSswKBgHX0\nVml4df9+PLHHoWVCCpdxP6pNKZL+OL7wNx4h0OJ0Ven0PIZkNXP1tAglsvre0aq0\nQgRMlcpf1TVDIH6fmHZWJc+KEoq5hd4QOyiCAH66XIIr7SsQAupgHD2eFDVPOtdy\n8LVu+cuCt6ZOxXgbeNKrBiCGUK9Is6LiRIRG8axzAoGAUV1fo/YJw2dWPbZqitwM\nXASbpy36/AV+MF0HCu/j/F/w6IKHrXrKY1pkcwiJdXgZIthPjzwFuB4/oXznO+KR\nVzKX4D9TWZLX3fSWvQEwwjGxC1KrAbzCpHImjqQJu8shr16MbeZs80g4Rsm0TVMF\nItmJiWW2U8ntrTFL4BRTfq8=\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-bpubd@dpool-e2d7a.iam.gserviceaccount.com",
+  "client_id": "115134757163628359356",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-bpubd%40dpool-e2d7a.iam.gserviceaccount.com"
+};
+exports.COOKIE_ENCRYPTION_KEY = 'SECRET';
+exports.OAUTH = {"web":{"client_id":"637598835406-9eckmrd8penj2ter5er0n2k461mgdft8.apps.googleusercontent.com","project_id":"3dpool","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://oauth2.googleapis.com/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"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","redirect_uris":["https://young-escarpment-72389.herokuapp.com/api/auth/google/redirect"]}};
+exports.DEPLOYED_URLS = {
+  // TODO: change to Heroku and GitLab URLs, with a trailing slash
+  SERVER: 'https://young-escarpment-72389.herokuapp.com/',
+  VIEW: 'https://young-escarpment-72389.herokuapp.com/'
+}
