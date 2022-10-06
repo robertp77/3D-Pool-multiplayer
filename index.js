0fe10c5432c30a41c69d8eb4da640d2d14375d0c
robertp77 <robert.a.paul99@gmail.com>
2022-10-06 14:42:48 -0800

    test

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ app.get('/', (req, res) => res.render('pages/index'))
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 app.get('/api/findmatch',
     async (req, res) => {
-        let opp,turn=await DataStore.findmatch(req.user?.id,first=true)
-        res.status(100).send('test');
+        //let opp,turn=await DataStore.findmatch(req.user?.id,first=true)
+        res.status(100).send({'success' : true, 'result': 't'});
     });
 app.get('/api/sendshot',
     async (req, res) => {
